cb8a83221d4dcef7036b18cbaa0d7a2dc18fd6df
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-11 16:41:45 +0200

    main message

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
 export default function Home() {
-  return (
-    <>
-      <h1>Dobrodošli na web stranicu svih Sapundžića</h1>
-    </>
-  );
+    return (
+        <main>
+            <div>
+                <h1>Dobrodošli na web stranicu porodice Sapundžić</h1>
+                <p>Ova stranica je trenutno u izradi, molimo vas da se vratite kasnije </p>
+                <p>LP,</p>
+                <p>Član porodice</p>
+            </div>
+
+        </main>
+    );
 }
