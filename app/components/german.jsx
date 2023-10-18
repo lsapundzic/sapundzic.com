bf8dc4401611eb4da55a74a9e83c8a61b5c35a2d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-18 15:01:39 +0200

    all language pages

---
+++ app/components/german.jsx
@@ -0,0 +1,19 @@
+const German = () => {
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Willkommen </h1>
+            <p className="py-6">
+              auf der Webseite der Familie Sapundzic. Diese Seite befindet sich
+              derzeit in der Produktion, bitte kommen Sie später wieder.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default German;
---
+++ app/components/slovenian.jsx
@@ -0,0 +1,19 @@
+const Slovenian = () => {
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Dobrodošli </h1>
+            <p className="py-6">
+              na spletni strani družine Sapundžić. Ta stran je trenutno v
+              izdelavi, prosimo, da se vrnete kasneje.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Slovenian;
