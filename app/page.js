e77f759c04203b381fcda82e0482d0a209249a3c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-18 15:22:24 +0200

    adding language control

--- app/page.js
+++ app/page.js
@@ -1,12 +1,24 @@
+"use client";
+
 import Serbian from "./components/serbian";
+import German from "./components/german";
+import Slovenian from "./components/slovenian";
+import { useState } from "react";
 
 export default function Home() {
+  const [lang, setLang] = useState(0);
+
   return (
     <main>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <Serbian />
+            <button
+              className="btn btn-primary"
+              onClick={() => setLang(lang + 1)}
+            >
+              {lang}
+            </button>
           </div>
         </div>
       </div>
