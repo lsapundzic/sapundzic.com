f4a9f17285809ec4410c41000ba5cc6444f3eb8d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-18 14:46:26 +0200

    Dodat nemacki

--- app/page.js
+++ app/page.js
@@ -1,13 +1,24 @@
 export default function Home() {
-    return (
-        <main>
-            <div>
-                <h1>Dobrodošli na web stranicu porodice Sapundžić</h1>
-                <p>Ova stranica je trenutno u izradi, molimo vas da se vratite kasnije </p>
-                <p>LP,</p>
-                <p>Član porodice</p>
-            </div>
+  return (
+    <main>
+      <div>
+        <h1>Dobrodošli na web stranicu porodice Sapundžić</h1>
+        <p>
+          Ova stranica je trenutno u izradi, molimo vas da se vratite kasnije{" "}
+        </p>
+        <p>LP,</p>
+        <p>Član porodice</p>
+      </div>
 
-        </main>
-    );
+      <div>
+        <h1>Willkommen auf der Website der Familie Sapundzic</h1>
+        <p>
+          Diese Webseite ist derzeit in Produktion, bitte besuchen Sie sie
+          später wieder.
+        </p>
+        <p>Mit freundlichen Grüßen, </p>
+        <p>Ein Familienmitglied</p>
+      </div>
+    </main>
+  );
 }
