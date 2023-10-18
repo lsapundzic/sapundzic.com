8730668ef2d7de5c58faba86b598edf6a083abfb
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-18 15:29:52 +0200

    Language button implemented

--- app/page.js
+++ app/page.js
@@ -8,16 +8,31 @@ import { useState } from "react";
 export default function Home() {
   const [lang, setLang] = useState(0);
 
+  let langComponent;
+  let langButton;
+
+  if (lang % 3 === 0) {
+    langComponent = <Serbian />;
+    langButton = "Deutsch";
+  } else if (lang % 3 === 1) {
+    langComponent = <German />;
+    langButton = "Slovenščina";
+  } else {
+    langComponent = <Slovenian />;
+    langButton = "Srpski";
+  }
+
   return (
     <main>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content text-center">
           <div className="max-w-md">
+            {langComponent}
             <button
               className="btn btn-primary"
               onClick={() => setLang(lang + 1)}
             >
-              {lang}
+              {langButton}
             </button>
           </div>
         </div>
