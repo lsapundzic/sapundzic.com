45cf346faf3a6d3f2bb491faad288210d5abe117
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-19 14:35:54 +0200

    ui

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sapundzic",
+  title: "Sapundzic Family",
   description: "Welcome to the Sapundzic Family website",
 };
 
