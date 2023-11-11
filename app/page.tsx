14b620fc8964a0ac3bf921882810e513be5dc1e4
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-11 22:56:19 +0100

    daisyUI works well

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <div>
       <h1>The Sapundzic Family Website</h1>
+      <div>
+        <h2>Testing Daisy</h2>
+        <button className="btn btn-primary">Hi</button>
+      </div>
     </div>
   );
 }
