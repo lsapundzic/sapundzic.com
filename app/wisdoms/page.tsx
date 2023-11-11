538b373447a5c3109991172335a3dce73fdd8e24
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-12 00:17:28 +0100

    added the wisdoms page

---
+++ app/wisdoms/page.tsx
@@ -0,0 +1,7 @@
+export default function Wisdoms() {
+  return (
+    <div>
+      <h1>Welcome to the wisdoms page!</h1>
+    </div>
+  );
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,32 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <Link href="./" className="btn btn-ghost normal-case text-xl">
+          Sapundzic
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <Link href="./wisdoms">Wisdoms</Link>
           </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
+          {/* For creating a dropdown menu*/}
+          {/*<li>*/}
+          {/*  <details>*/}
+          {/*    <summary>Parent</summary>*/}
+          {/*    <ul className="p-2 bg-base-100">*/}
+          {/*      <li>*/}
+          {/*        <a>Link 1</a>*/}
+          {/*      </li>*/}
+          {/*      <li>*/}
+          {/*        <a>Link 2</a>*/}
+          {/*      </li>*/}
+          {/*    </ul>*/}
+          {/*  </details>*/}
+          {/*</li>*/}
         </ul>
       </div>
     </div>
