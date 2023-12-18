e5bf9dccdeeda1e326b03320d7c30485810b9cf9
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-12-18 13:13:24 +0100

    introduced about g

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+export default function About() {
+  return (
+    <div>
+      <h1>Overview</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+        culpa, cumque enim molestias quibusdam sed! Doloremque ea eaque eveniet
+        magni omnis perferendis provident repellendus. Ad aliquam asperiores
+        autem beatae consectetur consequuntur dolore eligendi, ex magnam nihil
+        nisi omnis perspiciatis quod suscipit unde vel voluptatum? Accusamus
+        alias, autem, blanditiis distinctio dolor dolorem earum eius illo
+        maiores molestiae necessitatibus odit pariatur placeat praesentium quae
+        quidem quis sed totam! Ab amet consectetur consequatur enim eos, facilis
+        iste iure neque quod, ratione repellat sed ut velit vitae voluptate?
+        Assumenda cum dolor ea eligendi et fuga fugit id in libero porro
+        possimus, quia, similique veniam?
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-gray-900">The Sapundzic Family</h1>
-      <h2 className="text-xl font-medium text-gray-800">Offical Web Page</h2>
-    </div>
+    <Link href="./about">
+      <div className="flex justify-center items-center h-screen bg-gray-100">
+        <h1 className="text-6xl font-bold text-gray-900">
+          The Sapundzic Family
+        </h1>
+        <h2 className="text-xl font-medium text-gray-800">Offical Web Page</h2>
+      </div>
+    </Link>
   );
 }
