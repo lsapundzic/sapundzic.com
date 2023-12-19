a1c1de950e8900823bfd6c1a0df6c8a905baf741
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-12-19 14:46:45 +0100

    added a list of quotes

--- app/home/wisdom/page.tsx
+++ app/home/wisdom/page.tsx
@@ -1,7 +1,61 @@
 export default function Wisdom() {
   return (
     <div>
-      <p>Wisdom</p>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Qote</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr className="hover">
+              <th>1</th>
+              <td>A</td>
+              <td>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+                fuga illum iusto nemo nobis nostrum pariatur recusandae
+                reprehenderit repudiandae sint! Accusantium doloribus eaque
+                impedit iure laboriosam obcaecati recusandae reprehenderit
+                tempore veritatis voluptatem? A architecto aspernatur cumque
+                debitis dolores, earum enim facilis hic impedit ipsam itaque
+                maxime modi natus nostrum odit officiis sunt vel, vero voluptate
+                voluptatum. Architecto iste magnam maxime nostrum quaerat quasi
+                sapiente veritatis vero. Aliquid dolorem earum excepturi magni
+                saepe. Dolorum illum non sit. A ad amet autem deleniti ducimus
+                eius eligendi esse eveniet laboriosam, neque nihil obcaecati
+                officiis quaerat ratione repellat repellendus sit sunt unde?
+                Enim, iure.
+              </td>
+            </tr>
+            {/* row 2 */}
+            <tr className="hover">
+              <th>2</th>
+              <td>B</td>
+              <td>Desktop Support Technician</td>
+            </tr>
+            {/* row 3 */}
+            <tr className="hover">
+              <th>3</th>
+              <td>C</td>
+              <td>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Corporis iste maxime numquam sunt veritatis. Deserunt distinctio
+                dolore eos error exercitationem laboriosam maxime, nobis omnis,
+                quod similique veniam voluptate? Alias aspernatur ducimus illo
+                non totam. Culpa debitis ducimus est hic ipsum minus neque
+                quaerat quia quod, quos rem soluta? Autem beatae doloribus
+                inventore, libero molestias nulla veniam voluptas? Amet
+                asperiores corporis cumque explicabo nobis perspiciatis quis?
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div className="p-2 bg-amber-300">
+      <div className="navbar bg-base-100 rounded">
         <div className="flex-1">
-          <Link href="./home" className="btn btn-ghost text-xl">
+          <Link href="/home" className="btn btn-ghost text-xl">
             Sap
           </Link>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">
             <li>
-              <Link href="./home/about">About</Link>
+              <Link href="/home/about">About</Link>
             </li>
             <li>
-              <Link href="./home/wisdom">Wisdom</Link>
+              <Link href="/home/wisdom">Wisdom</Link>
             </li>
           </ul>
         </div>
